697f73b49389380af5a5ba0dd646fcc348d272ef
Yacine300 <herrouelyacine7@gmail.com>
2024-07-20 22:22:01 +0100

    peoduct_model

---
+++ lib/models/product.dart
@@ -0,0 +1,11 @@
+class Product {
+  final String id;
+  final String name;
+  final double price;
+  final double rating;
+  Product(
+      {required this.id,
+      required this.name,
+      required this.price,
+      required this.rating});
+}
